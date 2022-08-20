079ef31db841b9203257dd2737a928db11e81dea
kyoko-nr <kyokokitagawa@me.com>
2022-08-20 17:24:37 +0900

    fixed my dictionary index

--- src/components/MyDictionary/DictIndex.tsx
+++ src/components/MyDictionary/DictIndex.tsx
@@ -1,47 +1,30 @@
-import { useEffect, useState } from 'react'
-import { PosOptions, AlphabetOptions } from 'constants/Parts'
-import { Option, SortOption } from 'types/types'
-import { RowGridContainer, SimpleLink } from 'components/UIKit'
+import { Option } from 'types/types'
+import { SimpleLink } from 'components/UIKit'
+import { Stack } from '@mui/material'
 
 type DictIndexProps = {
-  sortOption: SortOption
   onClick: (option: Option) => void
+  options: readonly Option[]
+  selected: Option
 }
 
 const DictIndex = (props: DictIndexProps): JSX.Element => {
-  const [index, setIndex] = useState<Readonly<Option[]>>([])
-
-  useEffect(() => {
-    switch (props.sortOption.key) {
-      case '1':
-        setIndex(AlphabetOptions)
-        break
-      case '2':
-        setIndex(PosOptions)
-        break
-      default:
-        setIndex([])
-    }
-  }, [props.sortOption])
-
   return (
     <>
       <div className={'spacer-24'} />
-      {index.length > 0 && (
-        <RowGridContainer spacing={2} justifyContent="flex-start">
-          {index.map((option) => {
-            return (
-              <SimpleLink
-                label={option.value}
-                onClick={() => props.onClick(option)}
-                color="primary"
-                upperCase={true}
-                variant="body1"
-                disabled={option.key === props.sortOption.key}
-              />
-            )
-          })}
-        </RowGridContainer>
+      {props.options.length > 0 && (
+        <Stack spacing={2} direction="row">
+          {props.options.map((option) => (
+            <SimpleLink
+              label={option.value}
+              onClick={() => props.onClick(option)}
+              color="primary"
+              upperCase={true}
+              variant="body1"
+              disabled={option.key === props.selected.key}
+            />
+          ))}
+        </Stack>
       )}
     </>
   )
--- src/components/MyDictionary/MyDictContent.tsx
+++ src/components/MyDictionary/MyDictContent.tsx
@@ -5,10 +5,11 @@ import { DictIndex } from 'components/MyDictionary/index'
 import { WordCards } from 'components/UIKit/index'
 import { Word, Option, SortOption } from 'types/types'
 import { sortBy } from 'lodash'
+import { AlphabetOptions, PosOptions } from 'constants/Parts'
 
 type MyDictContentProps = {
   sortOption: SortOption
-  defaultFilterOption: Option
+  defaultIndex: Option
 }
 
 const MyDictContent = (props: MyDictContentProps): JSX.Element => {
@@ -16,43 +17,55 @@ const MyDictContent = (props: MyDictContentProps): JSX.Element => {
 
   const words = getWords(selector) as Word[]
   const [wordsToShow, setWordsToShow] = useState<Word[]>([])
+  const [index, setIndex] = useState<Option>(AlphabetOptions[0])
+  const [options, setOptions] = useState<Readonly<Option[]>>(AlphabetOptions)
 
-  const filterWords = (filter: Option): void => {
+  const filterWords = (index: Option): void => {
     let filtered: Word[] = []
     switch (props.sortOption.key) {
       case '1':
         // Alphabetical
-        filtered = words.filter((word) => word.title.startsWith(filter.value))
+        filtered = words.filter((word) => word.title.startsWith(index.value))
         setWordsToShow(sortBy(filtered, 'title'))
+        setOptions(AlphabetOptions)
+        setIndex(index)
         break
       case '2':
         // Parts of speech
-        filtered = words.filter((word) => word.pos === filter.key)
+        filtered = words.filter((word) => word.pos === index.key)
         setWordsToShow(sortBy(filtered, 'title'))
+        setOptions(PosOptions)
+        setIndex(index)
         break
       case '3':
         // Newer
         filtered = [...words]
         setWordsToShow(sortBy(filtered, 'createdAt').reverse())
+        setOptions([])
+        setIndex(index)
         break
       case '4':
         // Older
         filtered = [...words]
         setWordsToShow(sortBy(filtered, 'createdAt'))
+        setOptions([])
+        setIndex(index)
         break
       default:
         filtered = [...words]
+        setOptions([])
+        setIndex(index)
         break
     }
   }
 
   useEffect(() => {
-    filterWords(props.defaultFilterOption)
-  }, [props.defaultFilterOption])
+    filterWords(props.defaultIndex)
+  }, [props.defaultIndex])
 
   return (
     <>
-      <DictIndex sortOption={props.sortOption} onClick={filterWords} />
+      <DictIndex onClick={filterWords} options={options} selected={index} />
       <div className={'spacer-24'} />
       {wordsToShow.length > 0 ? <WordCards words={wordsToShow} withDate={true} /> : <div>No Words</div>}
     </>
